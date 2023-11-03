33c0ee3d8513c8ef95eeaeb9bc48a0d1bda1dda5
aitorpalomares <aitor.palomares@propylon.com>
2023-11-03 14:39:06 +0000

    form update

--- src/ui/Layout/FieldDetailsBoard/constants.ts
+++ src/ui/Layout/FieldDetailsBoard/constants.ts
@@ -1,20 +1,10 @@
 import { FIELD_TYPE } from "../../../../lib/main";
 import type { Field } from "../../../../lib/components/FormRenderer";
 
+const capitalizeFirstLetter = (st: string): string =>
+  st.charAt(0).toUpperCase() + st.slice(1);
+
 export const FORM_FIELDS: Field[] = [
-  {
-    name: "label",
-    label: "label",
-    type: FIELD_TYPE.TEXT,
-  },
-  {
-    name: "name",
-    label: "name",
-    type: FIELD_TYPE.TEXT,
-    validations: {
-      required: true,
-    },
-  },
   {
     name: "type",
     label: "type",
@@ -25,8 +15,31 @@ export const FORM_FIELDS: Field[] = [
     properties: {
       choices: Object.values(FIELD_TYPE).map((fieldType) => ({
         value: fieldType,
-        label: fieldType,
+        label: capitalizeFirstLetter(fieldType.replaceAll('_', ' ')),
       })),
     },
+  },
+  {
+    name: "name",
+    label: "name",
+    type: FIELD_TYPE.TEXT,
+    validations: {
+      required: true,
+    },
+  },
+  {
+    name: "label",
+    label: "label",
+    type: FIELD_TYPE.TEXT,
+  },
+  {
+    name: "placeholder",
+    label: "placeholder",
+    type: FIELD_TYPE.TEXT,
+  },
+  {
+    name: "disabled",
+    label: "disabled",
+    type: FIELD_TYPE.BOOLEAN,
   },
 ];
